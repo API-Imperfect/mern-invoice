cfab45faba4ac8ee6a1426278ff381d9bfd5e87f
API <alphaogilo@gmail.com>
2022-10-21 19:39:02 +0300

    chore: setup basic express backend api

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import chalk from "chalk";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.get("/api/v1/test", (req, res) => {
+	res.json({ Hi: "Welcome to the Invoice App" });
+});
+
+const PORT = process.env.PORT || 1997;
+
+app.listen(PORT, () => {
+	console.log(
+		`${chalk.green.bold("✔")} 👍 Server running in ${chalk.yellow.bold(
+			process.env.NODE_ENV
+		)} mode on port ${chalk.blue.bold(PORT)}`
+	);
+});
