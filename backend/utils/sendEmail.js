a25f90a30e997b8aed6c60afb15c6bcb89c193c6
API <alphaogilo@gmail.com>
2022-10-29 17:20:05 +0300

    feat: add sendEmail utility and email templates

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,33 @@
+import "dotenv/config";
+import fs from "fs";
+import handlebars from "handlebars";
+import path from "path";
+import { fileURLToPath } from "url";
+import transporter from "../helpers/emailTransport.js";
+import { systemLogs } from "./Logger.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const sendEmail = async (email, subject, payload, template) => {
+	try {
+		const sourceDirectory = fs.readFileSync(
+			path.join(__dirname, template),
+			"utf8"
+		);
+
+		const compiledTemplate = handlebars.compile(sourceDirectory);
+
+		const emailOptions = {
+			from: process.env.SENDER_EMAIL,
+			to: email,
+			subject: subject,
+			html: compiledTemplate(payload),
+		};
+		await transporter.sendEmail(emailOptions);
+	} catch (error) {
+		systemLogs.error(`email not sent: ${error}`);
+	}
+};
+
+export default sendEmail;
