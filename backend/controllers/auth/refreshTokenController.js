791b733db5c84e932ebfbbbb1cab45136dcbd087
API-Imperfect <alphaogilo@gmail.com>
2022-11-26 10:26:38 +0200

    feat: implemente access and refresh token rotation

---
+++ backend/controllers/auth/refreshTokenController.js
@@ -0,0 +1,107 @@
+import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import User from "../../models/userModel.js";
+
+// $-title   Get new access tokens from the refresh token
+// $-path    GET /api/v1/auth/new_access_token
+// $-auth    Public
+// we are rotating the refresh tokens, deleting the old ones, creating new ones and detecting token reuse
+
+const newAccessToken = asyncHandler(async (req, res) => {
+	const cookies = req.cookies;
+
+	if (!cookies?.jwt) {
+		return res.sendStatus(401);
+	}
+
+	const refreshToken = cookies.jwt;
+
+	const options = {
+		httpOnly: true,
+		maxAge: 24 * 60 * 60 * 1000,
+		secure: true,
+		sameSite: "None",
+	};
+	res.clearCookie("jwt", options);
+
+	const existingUser = await User.findOne({ refreshToken }).exec();
+
+	if (!existingUser) {
+		jwt.verify(
+			refreshToken,
+			process.env.JWT_REFRESH_SECRET_KEY,
+			async (err, decoded) => {
+				if (err) {
+					return res.sendStatus(403);
+				}
+				const hackedUser = await User.findOne({
+					_id: decoded.id,
+				}).exec();
+				hackedUser.refreshToken = [];
+				await hackedUser.save();
+			}
+		);
+		return res.sendStatus(403);
+	}
+
+	const newRefreshTokenArray = existingUser.refreshToken.filter(
+		(refT) => refT !== refreshToken
+	);
+
+	jwt.verify(
+		refreshToken,
+		process.env.JWT_REFRESH_SECRET_KEY,
+		async (err, decoded) => {
+			if (err) {
+				existingUser.refreshToken = [...newRefreshTokenArray];
+				await existingUser.save();
+			}
+
+			if (err || existingUser._id.toString() !== decoded.id) {
+				return res.sendStatus(403);
+			}
+
+			const accessToken = jwt.sign(
+				{
+					id: existingUser._id,
+					roles: existingUser.roles,
+				},
+				process.env.JWT_ACCESS_SECRET_KEY,
+				{ expiresIn: "1h" }
+			);
+
+			const newRefreshToken = jwt.sign(
+				{ id: existingUser._id },
+				process.env.JWT_REFRESH_SECRET_KEY,
+				{ expiresIn: "1d" }
+			);
+
+			existingUser.refreshToken = [
+				...newRefreshTokenArray,
+				newRefreshToken,
+			];
+			await existingUser.save();
+
+			const options = {
+				httpOnly: true,
+				maxAge: 24 * 60 * 60 * 1000,
+				secure: true,
+				sameSite: "None",
+			};
+
+			res.cookie("jwt", newRefreshToken, options);
+
+			res.json({
+				success: true,
+				firstName: existingUser.firstName,
+				lastName: existingUser.lastName,
+				username: existingUser.username,
+				provider: existingUser.provider,
+				avatar: existingUser.avatar,
+				accessToken,
+			});
+		}
+	);
+});
+
+export default newAccessToken;
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,6 +3,7 @@ import registerUser from "../controllers/auth/registerController.js";
 import verifyUserEmail from "../controllers/auth/verifyEmailController.js";
 import loginUser from "../controllers/auth/loginController.js";
 import { loginLimiter } from "../middleware/apiLimiter.js";
+import newAccessToken from "../controllers/auth/refreshTokenController.js";
 
 const router = express.Router();
 
@@ -11,5 +12,7 @@ router.post("/register", registerUser);
 router.get("/verify/:emailToken/:userId", verifyUserEmail);
 
 router.post("/login", loginLimiter, loginUser);
+
+router.get("/new_access_token", newAccessToken);
 
 export default router;
