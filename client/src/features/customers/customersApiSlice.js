1edcc01a1c85daa41c61e661e90ed00be362d6ae
API-Imperfect <alphaogilo@gmail.com>
2023-02-03 10:52:02 +0200

    feat: add CRUD Customer API Slices

---
+++ client/src/features/customers/customersApiSlice.js
@@ -0,0 +1,45 @@
+import { baseAPiSlice } from "../api/baseApiSlice";
+
+export const customersApiSlice = baseAPiSlice.injectEndpoints({
+	endpoints: (builder) => ({
+		getAllUserCustomers: builder.query({
+			query: (page = 1) => `/customer/all?page=${page}`,
+			providesTags: ["Customer"],
+		}),
+		createCustomer: builder.mutation({
+			query: (customerInfo) => ({
+				url: "/customer/create",
+				method: "POST",
+				body: customerInfo,
+			}),
+			invalidatesTags: ["Customer"],
+		}),
+		getSingleCustomer: builder.query({
+			query: (custId) => `/customer/${custId}`,
+			providesTags: ["Customer"],
+		}),
+		updateCustomerInfo: builder.mutation({
+			query: ({ id, ...otherFields }) => ({
+				url: `/customer/${id}`,
+				method: "PATCH",
+				body: otherFields,
+			}),
+			invalidatesTags: ["Customer"],
+		}),
+		deleteCustomer: builder.mutation({
+			query: (id) => ({
+				url: `/customer/${id}`,
+				method: "DELETE",
+			}),
+			invalidatesTags: ["Customer"],
+		}),
+	}),
+});
+
+export const {
+	useGetAllUserCustomersQuery,
+	useGetSingleCustomerQuery,
+	useCreateCustomerMutation,
+	useUpdateCustomerInfoMutation,
+	useDeleteCustomerMutation,
+} = customersApiSlice;
